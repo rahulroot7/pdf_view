9ce38bf1f797e314ad9af4b503a01dc5ffc5a2cc
Rahul chaurasia <76618247+rahulroot7@users.noreply.github.com>
2021-06-29 17:24:27 +0530

    Add files via upload

---
+++ dbtuts.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 29, 2021 at 01:54 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbtuts`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_uploads`
+--
+
+CREATE TABLE `tbl_uploads` (
+  `id` int(10) NOT NULL,
+  `file` varchar(100) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `size` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_uploads`
+--
+
+INSERT INTO `tbl_uploads` (`id`, `file`, `type`, `size`) VALUES
+(1, '68923-appointment_slip-(2).pdf', 'applicatio', 33),
+(2, '27005-in-purple-notes-for-sumit-(2).docx', 'applicatio', 13),
+(3, '24488-appointment_slip.pdf', 'applicatio', 33),
+(4, '76480-image_2021_06_18t10_50_53_599z.png', 'image/png', 719);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_uploads`
+--
+ALTER TABLE `tbl_uploads`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_uploads`
+--
+ALTER TABLE `tbl_uploads`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
